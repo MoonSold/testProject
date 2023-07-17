dfeb5f21466fae95be94380a585cd94c637764e9
Nikita <nikitasel0510@gmail.com>
2023-07-18 02:08:53 +0300

    fetch(src/Entity/House.php, src/Repository/HouseRepository.php, .env): Добавил сущность домов, подключение к базе

---
+++ src/Entity/House.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\HouseRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=HouseRepository::class)
+ */
+class House
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $bedrooms;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $bathrooms;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $storeys;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $garages;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getBedrooms(): ?int
+    {
+        return $this->bedrooms;
+    }
+
+    public function setBedrooms(int $bedrooms): self
+    {
+        $this->bedrooms = $bedrooms;
+
+        return $this;
+    }
+
+    public function getBathrooms(): ?int
+    {
+        return $this->bathrooms;
+    }
+
+    public function setBathrooms(int $bathrooms): self
+    {
+        $this->bathrooms = $bathrooms;
+
+        return $this;
+    }
+
+    public function getStoreys(): ?int
+    {
+        return $this->storeys;
+    }
+
+    public function setStoreys(int $storeys): self
+    {
+        $this->storeys = $storeys;
+
+        return $this;
+    }
+
+    public function getGarages(): ?int
+    {
+        return $this->garages;
+    }
+
+    public function setGarages(int $garages): self
+    {
+        $this->garages = $garages;
+
+        return $this;
+    }
+}
