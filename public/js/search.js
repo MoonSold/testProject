70ed387050d3ce896f1d81967e45412dd4f4088f
Nikita <nikitasel0510@gmail.com>
2023-07-18 12:38:13 +0300

    fetch(MainController, searchHouse.html.twig, search.js): Создана функция обработки нажатия кнопки, а также api поиска

--- public/js/search.js
+++ public/js/search.js
@@ -1,2 +1,21 @@
-$("#searchButton").on("click", function () {
-})
+$(document).ready(function() {
+    $("#searchButton").click(function() {
+        let data = {
+            "name":$("#inputHouseName").val(),
+            "bedrooms":$("#inputBedrooms").val(),
+            "bathrooms":$("#inputBathrooms").val(),
+            "garages":$("#inputGarages").val(),
+            "storeys":$("#inputStoreys").val(),
+            "costMin":$("#inputCostMin").val(),
+            "costMax":$("#inputCostMax").val()
+        }
+        $.ajax({
+            url: "/house/search",
+            type: "POST",
+            data: data,
+            success: function(response) {
+                searchCard(JSON.parse(response));
+            }
+        });
+    });
+});
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,7 +6,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use App\Repository\HouseRepository;
+use App\Entity\House;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+
 
 class MainController extends AbstractController
 {
@@ -20,11 +26,55 @@ class MainController extends AbstractController
     }
 
     /**
-     * @Route("/search", name="serchHouse"),
+     * @Route("/house", name="house"),
      */
-    public function serchHouse(HouseRepository $houseRepository)
+    public function house(HouseRepository $houseRepository)
     {
 
         return $this->render("searchHouse.html.twig", ["houses" => $houseRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/house/search", name="search"),
+     */
+    public function serchHouse(Request $request, SerializerInterface $serializer) {
+        if (!$request->isMethod('POST')) {
+            return $this->redirect("index");
+        }
+        $entityManager = $this->getDoctrine()->getManager();
+        $houseName = trim($request->request->get('name'));
+        $bedrooms = $request->request->get('bedrooms');
+        $bathrooms = $request->request->get('bathrooms');
+        $garages = $request->request->get('garages');
+        $storeys = $request->request->get('storeys');
+        $costMin = trim($request->request->get('costMin'));
+        $costMax = trim($request->request->get('costMax'));
+        $queryBuilder = $entityManager->createQueryBuilder();
+        $queryBuilder->select('h')
+            ->from(House::class, 'h');
+        if ($houseName) {
+            $queryBuilder->andWhere('h.name LIKE :name')->setParameters(['name' => '%'.$houseName.'%']);
+        }
+        if ($costMin) {
+            $queryBuilder->andWhere('h.cost >= :costMin')->setParameters(['costMin' => $costMin]);
+        }
+        if ($costMax) {
+            $queryBuilder->andWhere('h.cost <= :costMax')->setParameters(['costMax' => $costMax]);
+        }
+        if ($bedrooms !== '#') {
+            $queryBuilder->andWhere('h.bedrooms = :bedrooms')->setParameter('bedrooms', $bedrooms);
+        }
+        if ($bathrooms !== '#') {
+            $queryBuilder->andWhere('h.bathrooms = :bathrooms')->setParameter('bathrooms', $bathrooms);
+        }
+        if ($garages !== '#') {
+            $queryBuilder->andWhere('h.garages = :garages')->setParameter('garages', $garages);
+        }
+        if ($storeys !== '#') {
+            $queryBuilder->andWhere('h.storeys = :storeys')->setParameter('storeys', $storeys);
+        }
+        $houses = $queryBuilder->getQuery()->getResult();
+        $result = $serializer->serialize($houses, 'json', [AbstractNormalizer::IGNORED_ATTRIBUTES => ['id']]);
+        return new Response($result);
     }
 }
